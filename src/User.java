929d0167adb40a64aa3a341772b63ab96b9af440
Łukasz Krzewiński <korybutwiniowiecki@gmail.com>
2017-08-06 17:10:06 +0200

    Create User class.
    Has been create User class that will be hold all information about client such like name, surname, avatar, earned points etc.

---
+++ src/User.java
@@ -0,0 +1,111 @@
+import javax.swing.*;
+
+/**
+ * Created by Korybut on 06.08.2017.
+ */
+public class User {
+
+    private String login;
+    private char[] PASSWORD;
+    private String name;
+    private String surname;
+    private int points;
+    private int passExam;
+    private int failExam;
+    private int doneExam;
+    private ImageIcon avatar;
+
+    public User(String login, char[] PASSWORD, String name, String surname) {
+        this.login = login;
+        this.PASSWORD = PASSWORD;
+        this.name = name;
+        this.surname = name;
+    }
+
+    public User(String login, char[] PASSWORD, String name, String surname, int points, int passExam, int failExam, int doneExam) {
+        this.login = login;
+        this.PASSWORD = PASSWORD;
+        this.name = name;
+        this.surname = surname;
+        this.points = points;
+        this.passExam = passExam;
+        this.failExam = failExam;
+        this.doneExam = doneExam;
+    }
+
+    public User(String login, char[] PASSWORD, String name, String surname, int points, int passExam, int failExam, int doneExam, ImageIcon avatar) {
+        this.login = login;
+        this.PASSWORD = PASSWORD;
+        this.name = name;
+        this.surname = surname;
+        this.points = points;
+        this.passExam = passExam;
+        this.failExam = failExam;
+        this.doneExam = doneExam;
+        this.avatar = avatar;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public char[] getPASSWORD() {
+        return PASSWORD;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public int getPassExam() {
+        return passExam;
+    }
+
+    public int getFailExam() {
+        return failExam;
+    }
+
+    public int getDoneExam() {
+        return doneExam;
+    }
+
+    public ImageIcon getAvatar() {
+        return avatar;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public void setPassExam(int passExam) {
+        this.passExam = passExam;
+    }
+
+    public void setFailExam(int failExam) {
+        this.failExam = failExam;
+    }
+
+    public void setDoneExam(int doneExam) {
+        this.doneExam = doneExam;
+    }
+
+    public void setAvatar(ImageIcon avatar) {
+        this.avatar = avatar;
+    }
+}
